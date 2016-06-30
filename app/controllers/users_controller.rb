3aef95b58e6df3d04e54a09acae7bf7cc1909952
yoshihitosatou <yoshihito.s@to-do.co.jp>
2016-06-30 12:39:06 +0000

    add column user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, :set_user, :auth_user, only: [:edit, :update]
   def show
    @user = User.find(params[:id])
   end
@@ -17,11 +18,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      flash[:success] = "プロフィールを編集しました"
+      redirect_to @user
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location,
+                                 :profile )
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def auth_user
+    redirect_to root_url unless current_user == @user
   end
 end
 
